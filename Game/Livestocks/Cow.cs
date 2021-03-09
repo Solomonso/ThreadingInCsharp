63845a94d14e8454d7cf01a5058df7fce1cce582
Allan <akanyijuka@gmail.com>
2021-03-09 15:29:06 +0300

    Added Global, Main and various game states

--- Game/Livestocks/Cow.cs
+++ Game/Livestocks/Cow.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
 
 namespace ThreadingInCsharp.Game.Livestocks
 {
-    class Cow
+    class Cow : Livestock
     {
+        public Cow(Texture2D texture, Vector2 position) : base(texture, position, "cow", 4)
+        {
+
+        }
     }
 }
