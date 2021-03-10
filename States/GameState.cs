613383899f02fd9c20768f5fbed60a6344cc714a
solomon <Solomonn135@gmail.com>
2021-03-10 21:01:53 +0100

    Did inventory slot where farmer can keep track of the items sold

--- States/GameState.cs
+++ States/GameState.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using ThreadingInCsharp.Game;
+using ThreadingInCsharp.Game.Controls;
+using ThreadingInCsharp.Game.Items;
+using ThreadingInCsharp.Game.Map;
 
 namespace ThreadingInCsharp.States
 {
-    class GameState
+    public class GameState
     {
+       
     }
 }
