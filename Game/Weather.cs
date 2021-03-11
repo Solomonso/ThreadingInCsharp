f185f359f916aa330ce5d7fa97703c044026e20a
jonathanmohamed <jonathanmohamed34@gmail.com>
2021-03-11 09:44:20 +0100

    Created the weather.
    Added the weather.

--- Game/Weather.cs
+++ Game/Weather.cs
@@ -4,7 +4,55 @@ using System.Text;
 
 namespace ThreadingInCsharp.Game
 {
-    class Weather
-    {
-    }
+	public class Weather
+	{
+		int[] temperature;
+		int[] humidity;
+		int[] sunshine;
+		int value;
+
+		public Weather()
+		{
+			temperature = new int[] { 5, 10, 15, 20, 25, 30, 35 };
+			humidity = new int[] { 20, 30, 40, 50, 60, 70, 80 };
+			sunshine = new int[] { 20, 25, 30, 35, 40, 50, 55 };
+			value = 0;
+		}
+
+		public int randomTemp()
+		{
+			Random random = new Random();
+			int index = random.Next(0, 7);
+
+			return value + temperature[index];
+		}
+
+		public int randomHumidity()
+		{
+			Random random = new Random();
+			int index = random.Next(0, 7);
+			return value + humidity[index];
+		}
+
+		public int randomSun()
+		{
+			Random random = new Random();
+			int index = random.Next(0, 7);
+			return value + sunshine[index];
+		}
+
+		public bool randomRain()
+		{
+			Random random = new Random();
+			int index = random.Next(0, 2);
+			if (index == 1)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
 }
