c0cf8f53d3b531e7c6fb773f2ae313ecf67a44d6
IsaacSxm <rhymefirst1@gmail.com>
2021-03-11 20:59:31 +0100

    Built abstract state class

--- States/State.cs
+++ States/State.cs
@@ -1,10 +1,49 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using ThreadingInCsharp.Game;
 
 namespace ThreadingInCsharp.States
 {
-    class State
+    public abstract class State
     {
+        protected List<Entity> components;
+        protected ContentManager _content;
+
+        protected Global _global;
+
+        protected GraphicsDevice _graphicsDevice;
+
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            foreach (Entity component in components)
+            {
+                component.Draw(gameTime, spriteBatch);
+            }
+            spriteBatch.End();
+        }
+
+        public abstract void PostUpdate(GameTime gameTime);
+
+        public State(Global game, GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            _global = game;
+            _graphicsDevice = graphicsDevice;
+            _content = content;
+            this.components = new List<Entity>();
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+            foreach (Entity component in components)
+            {
+                components.Update(gameTime);
+            }
+            
+        }
     }
 }
