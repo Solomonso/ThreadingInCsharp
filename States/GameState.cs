464cf96edb8560de5c2298d4405327f23292adf0
Allan <akanyijuka@gmail.com>
2021-03-15 14:12:20 +0300

    clean code

--- States/GameState.cs
+++ States/GameState.cs
@@ -317,36 +317,6 @@ namespace ThreadingInCsharp.States
                     cowCount += 1;
                 }
             });
-
-            //if (animal.GetName() == "chicken")
-            //{
-            //    //Start new task immediately and grow chicken
-            //    Task.Factory.StartNew(() =>
-            //    {
-            //        Chicken chick = new Chicken(walkingChicken, new Vector2(500, 220));
-            //        components.Add(chick);
-            //        chick.Click += Livestock_Click;
-            //        i++;
-            //        chickenCount++;
-            //    });
-
-            //}
-            //else if (animal.GetName() == "cow")
-            //{
-            //    //Start new task immediately and grow cows
-            //    Task.Factory.StartNew(() =>
-            //    {
-            //        Cow cow = new Cow(walkingCow, new Vector2(420, 200));
-            //        components.Add(cow);
-            //        cow.Click += Livestock_Click;
-            //        i++;
-            //        cowCount += 1;
-            //    });
-
-            //}
-
-
-
         }
 
         void MouseMethod()
