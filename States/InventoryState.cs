c407593bdb38486d87c1738be4b9046e414a1211
jonathanmohamed <jonathanmohamed34@gmail.com>
2021-03-27 23:57:32 +0100

    updated inventorystate.cs
    Co-Authored-By: VanThuyVu <47418855+VanThuyVu@users.noreply.github.com>

--- States/InventoryState.cs
+++ States/InventoryState.cs
@@ -3,7 +3,6 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using ThreadingInCsharp.Game;
 using ThreadingInCsharp.Game.Controls;
 using ThreadingInCsharp.Game.interfaces;
