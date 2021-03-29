2056884cc92d9db37fbc1786a685f868ee3c7ba3
Allan <akanyijuka@gmail.com>
2021-03-29 14:27:11 +0300

    Hot Fix

--- States/InventoryState.cs
+++ States/InventoryState.cs
@@ -143,7 +143,7 @@ namespace ThreadingInCsharp.States
 
         };
 
-            Task.WaitAll(allTasks);
+            Task.WaitAll(allTasks); ////blocks the current thread until all other tasks have completed execution
         }
 
         private void GenerateSlot(Vector2 position, IInventoryItem item)
